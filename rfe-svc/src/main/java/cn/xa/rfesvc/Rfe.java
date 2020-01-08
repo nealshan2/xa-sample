be846dfc7eb7402405714d7e57edb3b86d9da787
neals <shandeai520@gmail.com>
2020-01-07 19:15:44 -0800

    fix bug for constructor

--- rfe-svc/src/main/java/cn/xa/rfesvc/Rfe.java
+++ rfe-svc/src/main/java/cn/xa/rfesvc/Rfe.java
@@ -1,9 +1,12 @@
 package cn.xa.rfesvc;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 
@@ -13,9 +16,12 @@ import javax.persistence.Id;
  */
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Rfe {
     @Id
+    @GeneratedValue
     private Long id;
     private String title;
     private String reference;
