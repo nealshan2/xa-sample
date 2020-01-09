e47e93c896f133ebec5e0a1d8845f36d21900d8a
neals <shandeai520@gmail.com>
2020-01-08 18:49:03 -0800

    fix build issue

--- job-api/src/test/java/cn/xa/AppTest.java
+++ job-api/src/test/java/cn/xa/AppTest.java
@@ -1,20 +1,20 @@
 package cn.xa;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+@SpringBootTest
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
 }
--- job-svc/src/test/java/cn/xa/AppTest.java
+++ job-svc/src/test/java/cn/xa/AppTest.java
@@ -1,20 +1,19 @@
 package cn.xa;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+@SpringBootTest
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
     }
 }
