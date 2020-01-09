c4b66462d7e0f98fcb56f61a9fe7393e4bf34f00
neals <shandeai520@gmail.com>
2020-01-08 22:30:50 -0800

    fix no default constructor issue

--- job-api/src/main/java/cn/xa/job/JobDto.java
+++ job-api/src/main/java/cn/xa/job/JobDto.java
@@ -1,7 +1,9 @@
 package cn.xa.job;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Neal Shan
@@ -9,6 +11,8 @@ import lombok.Data;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JobDto {
     private Long id;
     private String title;
