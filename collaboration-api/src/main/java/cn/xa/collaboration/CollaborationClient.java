033f8e9a040c3adc0b59a495918230d9a60187a4
neals <shandeai520@gmail.com>
2020-05-14 20:09:08 -0700

    save

--- collaboration-api/src/main/java/cn/xa/collaboration/CollaborationClient.java
+++ collaboration-api/src/main/java/cn/xa/collaboration/CollaborationClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @author Neal Shan
  * @since 0.0.1
  */
-@FeignClient(name = "collaborationClient", path = "/v1/collaboration", url = "http://localhost:8083")
+@FeignClient(name = "collaborationClient", path = "/v1/collaboration", url = "http://localhost:8088")
 public interface CollaborationClient {
 
     @PostMapping("/create")
