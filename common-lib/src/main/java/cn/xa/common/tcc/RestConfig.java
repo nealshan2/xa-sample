737ccbc925bb06626d90a64238905a1039594188
nealshen2 <shandeai520@gmail.com>
2020-05-16 12:37:54 +0800

    save rating

---
+++ common-lib/src/main/java/cn/xa/common/tcc/RestConfig.java
@@ -0,0 +1,19 @@
+package cn.xa.common.tcc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(60*1000);
+        requestFactory.setReadTimeout(60*1000);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        return restTemplate;
+    }
+}
--- task-svc/src/main/java/cn/xa/App.java
+++ task-svc/src/main/java/cn/xa/App.java
@@ -1,15 +1,15 @@
-package cn.xa.rating;
+package cn.xa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = {"cn.xa.collaboration", "cn.xa.tracking", "cn.xa.task"})
+@EnableFeignClients(basePackages = {"cn.xa.collaboration", "cn.xa.tracking"})
 public class App {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 
 }
